c2b0f5cde65699e4e5fc1aa544d007642765779c
Michael S <mihai.sanfran@gmail.com>
2019-06-01 21:13:41 -0700

    Connect SongList to Redux store and return list of songs as props

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class SongList extends Component {
     render() {
@@ -6,4 +7,10 @@ class SongList extends Component {
     }
 }
 
-export default SongList;
+// Take data inside Redux store, make calculation and return it as props inside this component
+const mapStateToProps = (state) => {
+    // Always return as object
+    return { songs: state.songs };
+}
+
+export default connect(mapStateToProps)(SongList);

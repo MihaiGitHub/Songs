7a756a27a5db8191504c57b9973bda05422c051c
Michael S <mihai.sanfran@gmail.com>
2019-06-02 16:28:20 -0700

    Render list of songs from Redux store

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+    renderList(){
+        // Produce array of songs and return it
+        return this.props.songs.map((song) => {
+            // Return some JSX for each song
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+
+                    <div className="content">
+                        {song.title}
+                    </div>
+                </div>
+            );
+        });
+    }
+
     render() {
-        return <div>SongList</div>;
+        return <div className="ui divided list">{this.renderList()}</div>;
     }
 }
 
